a05380549e733bdff63609471f2c404ab3c4e6d8
Jaycen <saitakusei@gmail.com>
2024-03-29 20:17:29 +0900

    Add scheme for mansion

--- src/schema.ts
+++ src/schema.ts
@@ -4,10 +4,51 @@ export const typeDefs = gql`
   type Query {
     hello: String
   }
+
+  type mansion {
+    category: String
+    name: String
+    address: String
+    station: String
+    description: String
+    image: String
+    url: String
+    price: String
+    size: String
+    age: String
+    updateDate: String
+  }
+
+  type mansionList {
+    mansions: [mansion]
+  }
+
+  type Query{
+    mansionList(name: String): mansionList
+  }
 `;
 
 export const resolvers = {
   Query: {
-    hello: () => 'Hello, GraphQL with Apollo!'
+    hello: () => 'Hello, GraphQL with Apollo!',
+    mansionList: () => {
+      return {
+        mansions: [
+          {
+            category: 'マンション',
+            name: 'ハイネスハイツ',
+            address: '東京都渋谷区代々木',
+            station: '代々木駅',
+            description: '代々木公園に隣接する好立地',
+            image: 'https://example.com/image.jpg',
+            url: 'https://example.com/',
+            price: '1,000万円',
+            size: '50m²',
+            age: '築10年',
+            updateDate: '2021-01-01',
+          },
+        ],
+      }
+    }
   }
 };
